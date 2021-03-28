fbf405c68d9b2abe0bc97200c7f1515266e25637
thierry_otvl <tbeigbeder.nospam@gmail.com>
2021-03-28 15:52:17 +0200

    publish otvl_web v2

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ This repository is used to publish blog articles and associated code.
 
 - code: contains the code associated with some articles
 - dev: files useful during development
-- docs: contains the blog articles to be served by the application "otvl_web"
+- blog: otvl web site and content for blog.otvl.org
---
+++ blog/otvl-web-app/src/components/pages/Err404.vue
@@ -0,0 +1,28 @@
+<template>
+
+      <div class="page-content p-2 w-full">
+        <div class="py-2 max-w-5xl mx-auto">
+          <h2>The page you requested was not found on this site.</h2>
+          <p>This error will be fixed, thanks for your understanding.</p>
+
+        </div>
+
+      </div>
+
+</template>
+
+<script>
+import { PageMixin } from 'otvl-web-lib'
+
+export default {
+  mixins: [PageMixin],
+
+  name: 'Err404',
+
+  data: function () {
+    return {
+      'localTitle': 'Page not found'
+    }
+  }
+}
+</script>
