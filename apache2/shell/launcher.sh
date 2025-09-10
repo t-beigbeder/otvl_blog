60a5613a7270d1618a7d97927fc456bb7616a5be
dvo <dvo@none.org>
2025-09-10 16:47:15 +0000

    cleanup

--- apache2/shell/launcher.sh
+++
@@ -1,4 +0,0 @@
-rm -f /srv/www/site1/web/sitemap.xml.gz
-sed -i -e "s=ows_ingress_host=${OWS_INGRESS_HOST}=" /srv/www/site1/web/robots.txt
-sed -i -e "s=ows_ingress_host=${OWS_INGRESS_HOST}=" /srv/www/site1/web/sitemap.xml
-exec httpd-foreground
